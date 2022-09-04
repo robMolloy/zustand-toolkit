14bda5ca8f41a92f8a00183a4c06f88c894317fa
robmolloyntt <robert.molloy@nttdata.com>
2022-09-04 21:32:16 +0100

    code and types in one file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export * from './toolkits';
+import { StoreApi } from 'zustand';
+
+export type TSimpleToolkit = {
+  data: unknown;
+  getData: () => void;
+  setData: (payload: unknown) => void;
+};
+
+export type TUseSimpleToolkit = TCreateStoreToolkit<TSimpleToolkit>;
+
+export type TGetState<T> = StoreApi<T>['getState'];
+export type TSetState<T> = StoreApi<T>['setState'];
+
+export type TCreateStoreToolkit<T> = (
+  set: TSetState<T>,
+  get: TGetState<T>
+) => T;
+
+export const useSimpleToolkit: TUseSimpleToolkit = (set, get) => ({
+  data: 0,
+  getData: () => get().data,
+  setData: payload => set({ data: payload }),
+});
--- src/toolkits/simpleToolkit/types/simpleToolkitTypes.d.ts
+++
@@ -1,9 +0,0 @@
-import { TCreateStoreToolkit } from '../../../types';
-
-export type TSimpleToolkit = {
-  data: unknown;
-  getData: () => void;
-  setData: (payload: unknown) => void;
-};
-
-export type TUseSimpleToolkit = TCreateStoreToolkit<TSimpleToolkit>;
